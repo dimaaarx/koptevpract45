7849676ed15e3f841ea57b857abdb73e0445232a
dimaaarx <koptevdima1107@gmail.com>
2025-03-26 07:45:20 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final String FILE_NAME = "textfile.txt";
+
+    public static void main(String[] args) {
+        showMenu();
+    }
+
+
+    public static void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nВиберіть дію :");
+            System.out.println("1. Записати в файл");
+            System.out.println("2. Прочитати файл");
+            System.out.println("3. Вихід");
+            System.out.print("Ваш вибір: ");
+
+            int choice;
+            try {
+                choice = scanner.nextInt();
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Помилка вводу ! Введите число.");
+                scanner.nextLine();
+                continue;
+            }
+
+            if (choice == 1) {
+                writeToFile();
+            } else if (choice == 2) {
+                readFromFile();
+            } else if (choice == 3) {
+                System.out.println("Вихід із програми.");
+                break;
+            } else {
+                System.out.println("Некоректний ввід! Спробуйте ще раз");
+            }
+        }
+        scanner.close();
+    }
+
+    public static void writeToFile() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть строку для запису в файл:");
+        String input = scanner.nextLine();
+
+        try (FileWriter writer = new FileWriter(FILE_NAME, true)) {
+            writer.write(input + "\n");
+            System.out.println("Запис успішно додано.");
+        } catch (IOException e) {
+            System.out.println("Помилка запису в файл!");
+        }
+    }
+
+    public static void readFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            System.out.println("\nВміст файла:");
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не знайден! Запишить данні спочатку");
+        } catch (IOException e) {
+            System.out.println("Помилка читання файлу!");
         }
     }
 }
